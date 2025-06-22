ca277fc4859469d35ef145ba31f1507bdff822f9
nastyahh <nasstyahus@gmail.com>
2025-06-22 13:18:31 +0300

    Updates

---
+++ assets/useScreenSize-Cy64_egk.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-BWot8QDf.js";const i=()=>{const[r,s]=t.useState(!1);return t.useEffect(()=>{const e=()=>{s(window.innerWidth<=768)};return e(),window.addEventListener("resize",e),()=>window.removeEventListener("resize",e)},[]),{isMobile:r}};export{i as u};
