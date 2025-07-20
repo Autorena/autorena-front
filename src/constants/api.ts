607381339275335c5626ac4e40c72b907a947558
Kostya <indianor201203@gmail.com>
2025-07-20 22:10:33 +0500

    feat: deploy on the new cluster (#9)

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
-export const API_HOST = "http://autorena.ru";
+export const API_HOST = "https://test.autorena.ru";
 export const CASDOOR_LOGIN_URL =
-  "http://87.228.81.144:30080/login/oauth/authorize?client_id=a6946f077f86f26fb1f5&response_type=code&redirect_uri=http://autorena.ru/api/authv3/api/v1/exchange_auth_code&scope=read&state=casdoor";
+  "https://auth-test.autorena.ru/login/oauth/authorize?client_id=a78a296c37da60699680&response_type=code&redirect_uri=https://test.autorena.ru/api/authv3/api/v1/exchange_auth_code&scope=read&state=casdoor";
 export const CASDOOR_REG_URL =
-  "http://87.228.81.144:30080/signup/oauth/authorize?client_id=a6946f077f86f26fb1f5&response_type=code&redirect_uri=http://autorena.ru/api/authv3/api/v1/exchange_auth_code&scope=read&state=casdoor";
+  "https://auth-test.autorena.ru/login/oauth/authorize?client_id=a78a296c37da60699680&response_type=code&redirect_uri=https://test.autorena.ru/api/authv3/api/v1/exchange_auth_code&scope=read&state=casdoor";
